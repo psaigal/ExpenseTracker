9ea4bb8d2e3cee933d30570cbbff33cce9f6b44a
psaigal <psaigal48@gmail.com>
2017-07-29 11:51:39 -0700

    Connect mongodb with node server

--- server.js
+++ server.js
@@ -9,3 +9,27 @@ app.get('/', function (req, res, next) {
 app.listen(port, '0.0.0.0', function() {
  console.log('Server running at port ' + port);
 });
+
+//Mongo Database
+var mongoose = require('mongoose'); //Place this on top
+
+/*Database connection - MongoDB*/
+
+//Created from the command earlier. Ensure this is done on the database instance
+var username = 'admin';
+var password = 'password';
+
+var dbHost = 'localhost';
+var dbPort = '27017';
+var database = 'database';
+
+var url = 'mongodb://' + username + ':' + password + '@' + dbHost + ':' + dbPort + '/' + database;
+console.log('mongodb connection = ' + url);
+
+mongoose.connect(url, function(err) {
+    if(err) {
+        console.log('connection error: ', err);
+    } else {
+        console.log('connection successful');
+    }
+});
